e55580768658c23e3b9ad0159c260ddd0454a81a
Feng Wang <wang_feng@live.com>
2020-12-18 15:36:02 +0100

    updated f16

--- float16_t.hpp
+++ float16_t.hpp
@@ -6,165 +6,220 @@
 #include <cmath>
 #include <bitset>
 
-namespace float16_t_private
+namespace numeric
 {
+    #ifdef DEBUG
+    constexpr unsigned debug_mode = 1;
+    #else
+    constexpr unsigned debug_mode = 0;
+    #endif
 
-    union float16
-	{
-        std::uint16_t bits_;
-		struct
-		{
-            std::uint16_t frac_ : 10;
-            std::uint16_t exp_  : 5;
-            std::uint16_t sign_ : 1;
-		} ieee_;
-	};
-
-    template< std::integral T >
-    constexpr float16 to_float16( T val ) noexcept
+    namespace float16_t_private
     {
-        return float16{ static_cast<std::uint16_t>(val) };
-    }
 
-    inline std::ostream& operator<<(std::ostream& os, float16 const& f16 )
-    {
-        os << std::bitset<1>(f16.ieee_.sign_) << " "
-           << std::bitset<5>(f16.ieee_.exp_) << " "
-           << std::bitset<10>(f16.ieee_.frac_);
-        return os;
-    }
-
-	union float32
-	{
-        std::uint32_t bits_;
-        struct
-		{
-			uint32_t frac_ : 23;
-			uint32_t exp_  : 8;
-			uint32_t sign_ : 1;
-		} ieee_;
-		float float_;
-	};
-
-    inline std::ostream& operator<<(std::ostream& os, float32 const& f32 )
-    {
-        os << std::bitset<1>(f32.ieee_.sign_) << " "
-           << std::bitset<8>(f32.ieee_.exp_) << " "
-           << std::bitset<23>(f32.ieee_.frac_) << " ("
-           << f32.float_ << ")";
-        return os;
-    }
-
-    constexpr float16 float32_to_float16( float input ) noexcept
-    {
-        using float16_t_private::to_float16;
-        float32 f32;
-        f32.float_ = input;
-        std::uint32_t const u32 = f32.bits_;
-
-        std::uint32_t sign = u32 & 0x80000000;
-        std::uint32_t exponent = u32 & 0x7f800000;
-        std::uint32_t coef = u32 & 0x7fffff;
-
-        if ( exponent == 0x7f800000 )
+        union float16
         {
-            std::uint32_t nan_bit = 0;
-            if ( coef != 0 )
-                nan_bit = 0x200;
-            return to_float16( (sign >> 16) | 0x7c00 | nan_bit | (coef >> 13) );
-        }
-
-        std::uint32_t half_sign = sign >> 16;
-        std::int32_t unbiased_exponent = static_cast<std::uint32_t>( exponent >> 23 ) - 127;
-        std::int32_t half_exponent = unbiased_exponent + 15;
-
-        if ( half_exponent == 0x1f )
-            return to_float16( half_sign | 0x7c00 );
-
-        if ( half_exponent <= 0 )
-        {
-            if ( half_exponent < -10 )
-                return to_float16( half_sign  );
-
-            std::uint32_t c = coef | 0x800000;
-            std::uint32_t half_coef = c >> ( 14 - half_exponent );
-            std::uint32_t round_bit = 1 << ( 13 - half_exponent );
-            if ( (c&round_bit) && (c & (3*round_bit-1)) )
-                ++half_coef;
-            return to_float16( half_sign | half_coef );
-        }
-
-        std::uint32_t uhalf_exponent = half_exponent << 10;
-        std::uint32_t half_coef = coef >> 13;
-        std::uint32_t round_bit = 0x1000;
-        if ( (coef & round_bit) && (coef&(3*round_bit-1)) )
-            return to_float16( (half_sign | uhalf_exponent | half_coef) + 1);
-        return to_float16( (half_sign | uhalf_exponent | half_coef) );
-    }
-
-    constexpr float32 float16_to_float32( std::uint16_t input ) noexcept
-    {
-        std::uint32_t sign = (input & 0x8000) << 16;
-        std::uint32_t exponent =  (input & 0x7c00) >> 10;
-        std::uint32_t coef = (input & 0x3ff) << 13;
-
-        if ( exponent == 0x1f )
-        {
-            if ( coef == 0 )
-                return float32{ sign | 0x7f800000 | coef };
-            return float32{ sign | 0x7fc00000 | coef };
-        }
-
-        if ( exponent == 0 )
-        {
-            if ( coef == 0 )
-                return float32{ sign };
-
-            ++exponent;
-            while( coef & 0x7f800000 )
+            std::uint16_t bits_;
+            struct
             {
-                coef <<= 1;
-                --exponent;
-            }
-            coef &= 0x7fffff;
+                std::uint16_t frac_ : 10;
+                std::uint16_t exp_  : 5;
+                std::uint16_t sign_ : 1;
+            } ieee_;
+        };
+
+        template< std::integral T >
+        constexpr float16 to_float16( T val ) noexcept
+        {
+            return float16{ static_cast<std::uint16_t>( val ) };
         }
 
-        return float32{ sign | (exponent+0x70) << 23 | coef };
-    }
+        inline std::ostream& operator<<( std::ostream& os, float16 const& f16 )
+        {
+            os << std::bitset<1>( f16.ieee_.sign_ ) << " "
+               << std::bitset<5>( f16.ieee_.exp_ ) << " "
+               << std::bitset<10>( f16.ieee_.frac_ );
+            return os;
+        }
 
-}//float16_private
+        union float32
+        {
+            std::uint32_t bits_;
+            struct
+            {
+                uint32_t frac_ : 23;
+                uint32_t exp_  : 8;
+                uint32_t sign_ : 1;
+            } ieee_;
+            float float_;
+        };
 
-struct float16_t
-{
-    float16_t_private::float16 data_;
+        inline std::ostream& operator<<( std::ostream& os, float32 const& f32 )
+        {
+            os << std::bitset<1>( f32.ieee_.sign_ ) << " "
+               << std::bitset<8>( f32.ieee_.exp_ ) << " "
+               << std::bitset<23>( f32.ieee_.frac_ ) << " ("
+               << f32.float_ << ")";
+            return os;
+        }
 
-    constexpr float16_t() noexcept = default;
-    constexpr float16_t( float16_t const& ) noexcept = default;
-    constexpr float16_t( float16_t && ) noexcept = default;
-    constexpr float16_t( float other ) : data_ { float16_t_private::float32_to_float16(other) } { }
+        inline constexpr float16 float32_to_float16( float input ) noexcept
+        {
+            using float16_t_private::to_float16;
+            float32 f32;
+            f32.float_ = input;
+            std::uint32_t const u32 = f32.bits_;
+            std::uint32_t sign = u32 & 0x80000000;
+            std::uint32_t exponent = u32 & 0x7f800000;
+            std::uint32_t coef = u32 & 0x7fffff;
 
-    constexpr float16_t& operator = ( float16_t const& ) noexcept = default;
-    constexpr float16_t& operator = ( float16_t && ) noexcept = default;
-    constexpr float16_t& operator = ( float other ) noexcept
+            if ( exponent == 0x7f800000 )
+            {
+                std::uint32_t nan_bit = 0;
+
+                if ( coef != 0 )
+                    nan_bit = 0x200;
+
+                return to_float16( ( sign >> 16 ) | 0x7c00 | nan_bit | ( coef >> 13 ) );
+            }
+
+            std::uint32_t half_sign = sign >> 16;
+            std::int32_t unbiased_exponent = static_cast<std::uint32_t>( exponent >> 23 ) - 127;
+            std::int32_t half_exponent = unbiased_exponent + 15;
+
+            if ( half_exponent == 0x1f )
+                return to_float16( half_sign | 0x7c00 );
+
+            if ( half_exponent <= 0 )
+            {
+                if ( half_exponent < -10 )
+                    return to_float16( half_sign  );
+
+                std::uint32_t c = coef | 0x800000;
+                std::uint32_t half_coef = c >> ( 14 - half_exponent );
+                std::uint32_t round_bit = 1 << ( 13 - half_exponent );
+
+                if ( ( c & round_bit ) && ( c & ( 3 * round_bit - 1 ) ) )
+                    ++half_coef;
+
+                return to_float16( half_sign | half_coef );
+            }
+
+            std::uint32_t uhalf_exponent = half_exponent << 10;
+            std::uint32_t half_coef = coef >> 13;
+            std::uint32_t round_bit = 0x1000;
+
+            if ( ( coef & round_bit ) && ( coef & ( 3 * round_bit - 1 ) ) )
+                return to_float16( ( half_sign | uhalf_exponent | half_coef ) + 1 );
+
+            return to_float16( ( half_sign | uhalf_exponent | half_coef ) );
+        }
+
+        inline constexpr float32 float16_to_float32( std::uint16_t input ) noexcept
+        {
+            std::uint32_t sign = ( input & 0x8000 ) << 16;
+            std::uint32_t exponent =  ( input & 0x7c00 ) >> 10;
+            std::uint32_t coef = ( input & 0x3ff ) << 13;
+
+            if ( exponent == 0x1f )
+            {
+                if ( coef == 0 )
+                    return float32{ sign | 0x7f800000 | coef };
+
+                return float32{ sign | 0x7fc00000 | coef };
+            }
+
+            if ( exponent == 0 )
+            {
+                if ( coef == 0 )
+                    return float32{ sign };
+
+                ++exponent;
+
+                while( coef & 0x7f800000 )
+                {
+                    coef <<= 1;
+                    --exponent;
+                }
+
+                coef &= 0x7fffff;
+            }
+
+            return float32{ sign | ( exponent + 0x70 ) << 23 | coef };
+        }
+
+    }//namespace float16_private
+
+    struct float16_t
     {
-        float16_t f{ other };
-        std::swap( *this, f );
-        return *this;
-    }
+        float16_t_private::float16 data_;
 
-    constexpr operator float() const noexcept
+        constexpr float16_t() noexcept = default;
+        constexpr float16_t( float16_t const& ) noexcept = default;
+        constexpr float16_t( float16_t&& ) noexcept = default;
+        constexpr float16_t( float other ) noexcept : data_ { float16_t_private::float32_to_float16( other ) } { }
+        constexpr float16_t( std::uint16_t bits ) noexcept : data_{ bits } { }
+
+        constexpr float16_t& operator = ( float16_t const& ) noexcept = default;
+        constexpr float16_t& operator = ( float16_t&& ) noexcept = default;
+        constexpr float16_t& operator = ( std::uint16_t bits ) noexcept
+        {
+            data_.bits_ = bits;
+            return *this;
+        }
+        constexpr float16_t& operator = ( float other ) noexcept
+        {
+            data_ = float16_t_private::float32_to_float16( other );
+            return *this;
+        }
+
+        constexpr operator float() const noexcept
+        {
+            auto f32 = float16_t_private::float16_to_float32( data_.bits_ );
+            return f32.float_;
+        }
+
+        constexpr operator std::uint16_t() const noexcept
+        {
+            return data_.bits_;
+        }
+    }; //struct float16_t
+
+    constexpr bool is_nan( float16_t f16 ) noexcept
     {
-        auto f32 = float16_t_private::float16_to_float32( data_.bits_ );
-        return f32.float_;
+        return (std::uint16_t(f16) & 0x7fff) > 0x7f80;
     }
 
-};
+    constexpr bool is_inf( float16_t f16 ) noexcept
+    {
+        return (std::uint16_t(f16) & 0x7fff) == 0x7f80;
+    }
 
-inline std::ostream& operator<< ( std::ostream& os, float16_t const& f )
-{
-    os  << std::bitset<1>(f.data_.ieee_.sign_) << " ";
-    os  << std::bitset<5>(f.data_.ieee_.exp_) << " ";
-    os  << std::bitset<10>(f.data_.ieee_.frac_);
-    return os << " --> " << float( f );
-}
+    constexpr bool is_isfinite( float16_t f16 ) noexcept
+    {
+        return (std::uint16_t(f16) & 0x7f80) != 0x7f80;
+    }
+
+    constexpr bool is_normal( float16_t f16 ) noexcept
+    {
+        auto const exponent = std::uint16_t(f16) & 0x7f80;
+        return (exponent != 0x7f80) && (exponent != 0);
+    }
+
+
+    inline std::ostream& operator << ( std::ostream& os, float16_t const& f )
+    {
+        os << float(f);
+        if constexpr( debug_mode )
+        {
+            os << "(";
+            os  << std::bitset<1>( f.data_.ieee_.sign_ ) << " ";
+            os  << std::bitset<5>( f.data_.ieee_.exp_ ) << " ";
+            os  << std::bitset<10>( f.data_.ieee_.frac_ ) << ")";
+        }
+        return os;
+    }
+
+
+}//namespace numeric
 
--- tests/test.cc
+++ tests/test.cc
@@ -1,21 +1,24 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "../float16_t.hpp"
-//#include "../half.hpp"
 #include <cmath>
 #include <iostream>
 #include <bitset>
 
 void print( float x )
 {
-    using float16_t_private::float32_to_float16;
-    using float16_t_private::float16_to_float32;
+    using numeric::float16_t_private::float32_to_float16;
+    using numeric::float16_t_private::float16_to_float32;
 
     auto f16 = float32_to_float16( x );
     std::cout << x << " => " << f16 << std::endl;
 
     auto f32 = float16_to_float32( f16.bits_ );
     std::cout << "f32 => " << f32 << std::endl;
+
+    //_Float16 _f16 = x;
+    //float _x = _f16 * 1.0f;
+    //std::cout << _x << std::endl;
 
     std::cout << "****************************" << std::endl;
 }
@@ -52,19 +55,23 @@ TEST_CASE( "32_2_16", "[32_2_16]" )
 
     print( 16504.0f  );
     print( -16204.0f  );
+
+    for ( float x = -1.0f; x <= 1.0f; x+= 0.013 )
+    {
+        numeric::float16_t f{x};
+        std::cout << x << " : " << f << std::endl;
+    }
 }
 
-/*
 TEST_CASE( "f1", "[f1]" )
 {
-    float const mx = 6504.0;
-    float const interval = mx / 100000.0;
+    float const mx = 10.0;
+    float const interval = mx / 1000.0;
     float x = -mx;
     for ( ; x < mx; x += interval )
     {
-        float16_t const f_{ x };
+        numeric::float16_t const f_{ x };
         float const x_ = float( f_ );
-        REQUIRE( std::abs(x- x_) < 1.0f );
+        REQUIRE( std::abs(x- x_) < 0.01f );
     }
 }
-*/
